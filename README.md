6be206487a551e989883d68088402958b5935b20
VCD <VeryCrazyDog@gmail.com>
2017-01-15 23:14:47 +0800

    Implement Sleep as Android conversion

---
+++ README.md
@@ -0,0 +1,57 @@
+# sleepbot-to-sleep_as_android
+
+This Python script convert [Sleepbot](https://www.mysleepbot.com/) records exported from the app to [Sleep as Android](http://sleep.urbandroid.org/) supported import format.
+
+This script is written for and tested in the following enviroment:
+* Python 3.5.2 64-bit on Windows 7 64-bit
+* Sleepbot 3.2.8 on Android 7.1.1
+* Sleep as Android 20161122 on Android 7.1.1
+
+## Usage
+
+1. In **Sleepbot**, go to the second tab for statistic, set the **From** date to the earliest record date and press the **SHARE** button at the bottom right of the screen. Take note on the exported file path.
+2. Copy the exported file to a computer with Python 3.5.
+3. Change the configuration in the script `sleepbot_to_sleep_as_android.py`. Refer to the below section for configurable options.
+4. Run the conversion script.
+5. On **Sleep as Android**, open the menu and select **Backup**, then click **Export data** from the pop up menu to backup the existing data. Take note on the exported file path.
+6. Copy the **Sleep as Android** backup to the computer as backup and replace it with the converted file.
+7. On **Sleep as Android**, open the menu and select **Backup**, then click **Import data** from the pop up menu to import the converted file.
+
+## Configurable Options
+
+There are several configuration set in the script which will need to change in order for the convertion to work.
+
+* `PATH_IN_SLEEPBOT_EXPORT`: The path to the **Sleepbot** exported file. On Windows, please make sure that the path is using slash `/` instead of backslash `\` for the directory separator.
+* `PATH_OUT_SLEEP_AS_ANDROID`: The path converted **Sleep as Android** file format for import. On Windows, please make sure that the path is using slash `/` instead of backslash `\` for the directory separator.
+* `SLEEPBOT_EXPORT_DATE_FORMAT`: The date format used in the **Sleepbot** exported file. You will have to change it if the date format in the exported file not `dd/mm/yy`.
+* `SLEEPBOT_EXPORT_TIME_FORMAT`: The time format used in the **Sleepbot** exported file. Most like you don't need to change it.
+* `TIMEZONE`: The timezone of records in the **Sleepbot** exported file. You will have to change it if `Asia/Hong_Kong` is not your timezone. You may refer to https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for the list of timezone available.
+* `NOTE_PREFIX`: A prefix to be added before your note of each sleep records. You will have to change it if you don't like `[Import from Sleepbot]` to be added for each of your record.
+
+## Sample Files
+
+**Sleepbot** sample exported file:
+
+```csv
+Date, Sleep Time, Wake Time, Hours,Note
+13/01/17,13:25,13:55,0.49,
+13/01/17,00:50,07:56,7.1,
+12/01/17,00:17,07:48,7.51,
+09/01/17,13:13,13:54,0.69,
+09/01/17,23:59,07:56,7.95,
+```
+
+Sample converted format compatible for import in **Sleep as Android**:
+
+```csv
+Id,Tz,From,To,Sched,Hours,Rating,Comment,Framerate,Snore,Noise,Cycles,DeepSleep,LenAdjust,Geo,"13:55"
+"1484285100000","Asia/Hong_Kong","13. 01. 2017 13:25","13. 01. 2017 13:55","13. 01. 2017 13:55","0.490","0.0","[Import from Sleepbot]","10000","-1","-1.0","-1","-2.0","0","","0.0"
+Id,Tz,From,To,Sched,Hours,Rating,Comment,Framerate,Snore,Noise,Cycles,DeepSleep,LenAdjust,Geo,"07:56"
+"1484239800000","Asia/Hong_Kong","13. 01. 2017 0:50","13. 01. 2017 7:56","13. 01. 2017 7:56","7.100","0.0","[Import from Sleepbot]","10000","-1","-1.0","-1","-2.0","0","","0.0"
+Id,Tz,From,To,Sched,Hours,Rating,Comment,Framerate,Snore,Noise,Cycles,DeepSleep,LenAdjust,Geo,"07:48"
+"1484151420000","Asia/Hong_Kong","12. 01. 2017 0:17","12. 01. 2017 7:48","12. 01. 2017 7:48","7.510","0.0","[Import from Sleepbot]","10000","-1","-1.0","-1","-2.0","0","","0.0"
+Id,Tz,From,To,Sched,Hours,Rating,Comment,Framerate,Snore,Noise,Cycles,DeepSleep,LenAdjust,Geo,"13:54"
+"1483938780000","Asia/Hong_Kong","09. 01. 2017 13:13","09. 01. 2017 13:54","09. 01. 2017 13:54","0.690","0.0","[Import from Sleepbot]","10000","-1","-1.0","-1","-2.0","0","","0.0"
+Id,Tz,From,To,Sched,Hours,Rating,Comment,Framerate,Snore,Noise,Cycles,DeepSleep,LenAdjust,Geo,"07:56"
+"1483891140000","Asia/Hong_Kong","08. 01. 2017 23:59","09. 01. 2017 7:56","09. 01. 2017 7:56","7.950","0.0","[Import from Sleepbot]","10000","-1","-1.0","-1","-2.0","0","","0.0"
+```
--- sleepbot-to-sleep_as_android/sleepbot_to_sleep_as_android.py
+++ sleepbot-to-sleep_as_android/sleepbot_to_sleep_as_android.py
@@ -1,22 +1,26 @@
 # Configuration
 PATH_IN_SLEEPBOT_EXPORT = "E:/Pending/Sleep/SleepBot/Exports/14_01_17_1484325789185.csv"
-PATH_IN_SLEEPBOT_EXPORT = "E:/Pending/Sleep/SleepBot/Exports/14_01_17_1484325789185_dev.csv"
-PATH_OUT_SLEEP_AS_ANDROID = ""
+PATH_OUT_SLEEP_AS_ANDROID = "E:/Pending/Sleep/SleepBot/Exports/14_01_17_1484325789185_out.csv"
 SLEEPBOT_EXPORT_DATE_FORMAT = "%d/%m/%y"
 SLEEPBOT_EXPORT_TIME_FORMAT = "%H:%M"
 TIMEZONE = "Asia/Hong_Kong"
+NOTE_PREFIX = "[Import from Sleepbot]"
 
 # Constants
 SLEEPBOT_EXPORT_HEADER = ["Date", "Sleep Time", "Wake Time", "Hours", "Note"]
+SLEEP_AS_ANDROID_TIME_FORMAT = '%H:%M'
+SLEEP_AS_ANDROID_DATETIME_FORMAT = '%d. %m. %Y %-H:%M'
+SLEEP_AS_ANDROID_DATETIME_FORMAT = '{dt:%d}. {dt:%m}. {dt:%Y} {dt.hour}:{dt:%M}'
 
 # Import
+import collections
 import csv
 from datetime import datetime, timedelta
+import decimal
 import math
-from collections import namedtuple
 
 # Define named tuple
-SleepbotExportRow = namedtuple('SleepbotExportRow', 'sleep_time wake_time note')
+SleepbotExportRow = collections.namedtuple('SleepbotExportRow', 'sleep_time wake_time hours note')
 
 # Functions implementation
 def check_header(row):
@@ -36,32 +40,77 @@ def parse_sleepbot_row(row):
     if len(row) == 5:
         # Trim all space characters
         map(str.strip, row)
-        # Parse the sleep time and wake time
+        # Parse the sleep time, wake time and sleep hours
         sleep_time = datetime.strptime(row[0] + ' ' + row[1], SLEEPBOT_EXPORT_DATE_FORMAT + ' ' + SLEEPBOT_EXPORT_TIME_FORMAT)
         wake_time = datetime.strptime(row[0] + ' ' + row[2], SLEEPBOT_EXPORT_DATE_FORMAT + ' ' + SLEEPBOT_EXPORT_TIME_FORMAT)
+        sleep_hours = decimal.Decimal(row[3])
         # Check whether the sleep time advanced some days, in Sleepbot, the date is the date of the wake time
         if wake_time < sleep_time:
             # Check the sleep hours, just in case someone sleep over 24 hours which will across 2 days
-            sleep_hours = float(row[3])
             day_adj = math.ceil(sleep_hours / 24)
             # Adjust the sleep time
             sleep_time -= timedelta(days = day_adj)
         # Build the result
-        result = SleepbotExportRow(sleep_time, wake_time, row[4])
+        result = SleepbotExportRow(sleep_time, wake_time, sleep_hours, row[4])
     else:
         print('WARN: Skipping invalid Sleepbot data', row)
     return result
 
-def process(row):
+def write_sleep_as_android_row(csv_writer, row):
+    csv_writer.writerow([
+        'Id',
+        'Tz',
+        'From',
+        'To',
+        'Sched',
+        'Hours',
+        'Rating',
+        'Comment',
+        'Framerate',
+        'Snore',
+        'Noise',
+        'Cycles',
+        'DeepSleep',
+        'LenAdjust',
+        'Geo',
+        '"' + datetime.strftime(row.wake_time, SLEEP_AS_ANDROID_TIME_FORMAT) + '"'
+    ])
+    csv_writer.writerow([
+        '"' + str(int(row.sleep_time.timestamp()) * 1000) + '"',
+        '"' + TIMEZONE + '"',
+        '"' + SLEEP_AS_ANDROID_DATETIME_FORMAT.format(dt = row.sleep_time) + '"',
+        '"' + SLEEP_AS_ANDROID_DATETIME_FORMAT.format(dt = row.wake_time) + '"',
+        '"' + SLEEP_AS_ANDROID_DATETIME_FORMAT.format(dt = row.wake_time) + '"',
+        '"' + '{0:.3f}'.format(row.hours) + '"',
+        '"0.0"',
+        '"' + NOTE_PREFIX + row.note + '"',
+        '"10000"',
+        '"-1"',
+        '"-1.0"',
+        '"-1"',
+        '"-2.0"',
+        '"0"',
+        '""',
+        '"0.0"'
+    ])
+
+def process(csv_writer, row):
     row = parse_sleepbot_row(row)
-    print(row)
+    write_sleep_as_android_row(csv_writer, row)
 
 # Main implementation
-with open(PATH_IN_SLEEPBOT_EXPORT, 'r') as in_file:
+with open(PATH_IN_SLEEPBOT_EXPORT, 'r') as in_file, open(PATH_OUT_SLEEP_AS_ANDROID, 'w', newline = '') as out_file:
     reader = csv.reader(in_file)
     header = next(reader, [])
     if check_header(header):
+        writer = csv.writer(out_file, quoting = csv.QUOTE_NONE, escapechar = '', quotechar = '')
+        count = 0
         for row in reader:
-            process(row)
+            process(writer, row)
+            count += 1
+            if count % 50 == 0:
+                print('Converted', count, 'records!' if count > 1 else 'record!')
+        print('Converted', count, 'records!' if count > 1 else 'record!')
+        print('Convertion finished!')
     else:
         print('ERROR: Header of Sleepbot export file is not correct, expected ', SLEEPBOT_EXPORT_HEADER, ', given ', header, sep = '')
